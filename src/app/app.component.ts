39a6684dd71b4131a27e916493271aa0ffaaf3fc
Milorad Komljenovic <Miloradk95@gmail.com>
2023-01-30 23:22:16 +0100

    izmena boja i templejta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
   }
 
   ngAfterViewInit() {
-    this.observer.observe(['(max-width: 800px)']).subscribe((res => {
+    this.observer.observe(['(max-width: 2000px)']).subscribe((res => {
       if(res.matches) {
         this.sidenav.mode = 'over';
         this.sidenav.close();
